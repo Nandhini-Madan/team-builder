8a380ab80ecf3db3ee49dda0b8d7889f5babfbe9
unknown <nandhinimadan20@gmail.com>
2020-07-10 18:41:13 -0700

    Task 1 completed

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,14 +1,26 @@
-import React,{useState} from 'React';
+import React, { useState } from 'react';
 
 const Form =props=>{
-
-    const[name,SetName]=useState("");
-    const[Email,SetEmail]=useState("");
-    const[role,SetRole]=useState("");
+    const [name, setName]=useState("");
+    const [email,SetEmail]=useState("");
+    const[job,Setjob]=useState("");
+    const handleName= event =>setName(event.target.value);
+    const handlesubmit=event=>
+    props.SetTeamList([...props.SetTeamList,{name}]);
+    console.log("set",props.SetTeamList);
     return(
-        <div></div>
+        <div>
+            <form onSubmit={handlesubmit}>
+
+            <input 
+                onChange={handleName}
+                type="text"
+                name="name"
+                value={name} >
+                </input>
+            </form>
+
+        </div>
     );
-
 };
-
 export default Form;
